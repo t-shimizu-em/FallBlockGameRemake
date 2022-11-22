edac0099a3635d480bbc7160b08de2e88d412e9c
t255kg <t.shimizu@extramile.jp>
2022-11-22 09:49:33 +0900

    ブロックプレハブの準備、生成初期位置の決定

--- Assets/Scripts/GameStatus.cs
+++ Assets/Scripts/GameStatus.cs
@@ -1,18 +1,32 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class GameStatus : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [HideInInspector] public int _fallBlockNum;
+    [HideInInspector] public int _nextFallBlockNum;
+    [HideInInspector] public int _gameStatus;
+
+    private const int GameOver = 0;
+    private const int Start = 1;
+    private const int Ground = 2;
+    private const int Erase = 3;
+
+    public void Init()
     {
-        
+        _fallBlockNum = Random.Range(0, 7);
+        _nextFallBlockNum = Random.Range(0, 7);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void StartProcess()
     {
-        
+        _fallBlockNum = _nextFallBlockNum;
+        _nextFallBlockNum = Random.Range(0, 7);
+
+
+    }
+
+    public void GroundProcess()
+    {
+
     }
 }
